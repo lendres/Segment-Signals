b2c21dbd1a9e53e9e029c86e7c4aa23abaa5798e
Lance A. Endres <lanceendres@gmail.com>
2023-02-13 13:22:36 -0600

    Remove debugging code.

--- src/Python Extension/module.cpp
+++ src/Python Extension/module.cpp
@@ -1,4 +1,3 @@
-//#include <Windows.h>
 #include <cmath>
 #include <pybind11/pybind11.h>
 #include <pybind11/numpy.h>
@@ -7,97 +6,24 @@
 #include "SegmentationResults.h"
 #include "SegmentationResultsPy.h"
 
-#include <iostream>
-#include <fstream>
-using namespace std;
-
 namespace py = pybind11;
-
-const double e = 2.7182818284590452353602874713527;
-
-double sinh_impl(double x)
-{
-    return (1 - pow(e, (-2 * x))) / (2 * pow(e, -x));
-}
-
-double cosh_impl(double x)
-{
-    return (1 + pow(e, (-2 * x))) / (2 * pow(e, -x));
-}
-
-double tanh_impl(double x)
-{
-    return sinh_impl(x) / cosh_impl(x);
-}
-
-struct Pet
-{
-    Pet(const std::string& name) : name(name) { }
-    void setName(const std::string& name_) { name = name_; }
-    const std::string& getName() const { return name; }
-
-    std::string name;
-};
-
-// Passing in an array of doubles
-py::array_t<double> twice(py::array_t<double> xs)
-{
-    py::buffer_info info = xs.request();
-    double* ptr = static_cast<double*>(info.ptr);
-
-    int n = 1;
-    for (int r: info.shape)
-    {
-        n *= r;
-    }
-
-    for (int i = 0; i <n; i++)
-    {
-        *ptr++ *= 2;
-    }
-
-    return xs;
-}
 
 PythonAlgorithms::SegmentationResults* Segment(py::array_t<double> signalAsPyList, int signalLength, double threshold, int jumpSequenceWindowSize, int noiseVarianceWindowSize)
 {
     py::buffer_info info        = signalAsPyList.request();
     double* signalDataPointer   = static_cast<double*>(info.ptr);
 
-
-
-	ofstream myfile;
-	myfile.open("C:\\Projects\\ddosi\\Segment Signals\\src\\Python Extension\\debugoutput.txt");
-	for (int i = 0; i < signalLength; i++)
-	{
-
-		myfile << signalDataPointer[i] << std::endl;
-	}
-	myfile.close();
-
-
-
     Algorithms::SegmentationResults*		cppResults		= Algorithms::SegmentSignal::Segment(signalDataPointer, signalLength, threshold, jumpSequenceWindowSize, noiseVarianceWindowSize);
     PythonAlgorithms::SegmentationResults*	pythonResults	= new PythonAlgorithms::SegmentationResults(cppResults);
 	
-	//delete cppResults;
+	delete cppResults;
 
     return pythonResults;
 }
 
-
 PYBIND11_MODULE(SegmentSignalPy, m)
 {
-    m.def("sinh_impl", &sinh_impl, "Compute a hyperbolic sin of a single argument expressed in radians.");
-    m.def("fast_tanh2", &tanh_impl, "Compute a hyperbolic tangent of a single argument expressed in radians.");
-    m.def("twice", &twice, "Double every element in a list.");
-
     m.def("Segment", &Segment, "Segments a signal based on maximum likelihood estimation");
-
-    py::class_<Pet>(m, "Pet")
-        .def(py::init<const std::string&>())
-        .def("setName", &Pet::setName)
-        .def("getName", &Pet::getName);
 
     //py::class_<Algorithms::SegmentSignal>(m, "SegmentSignal")
     //    .def_static("Segment", static_cast<Algorithms::SegmentationResults* (*)(double[], int, double, int, int)>(&Algorithms::SegmentSignal::Segment));
