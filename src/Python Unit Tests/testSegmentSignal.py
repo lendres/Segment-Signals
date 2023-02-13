dfc61d1c2c57ac30f28794183846d512499ded37
Lance A. Endres <lanceendres@gmail.com>
2023-02-13 13:48:24 -0600

    More unit tests for Python.

--- src/Python Unit Tests/testSegmentSignal.py
+++ src/Python Unit Tests/testSegmentSignal.py
@@ -9,6 +9,11 @@ from   NoiseVarianceEstimateMethod               import NoiseVarianceEstimateMet
 
 import unittest
 
+
+import matplotlib.pyplot                         as plt
+
+
+
 class TestSegmentSignal(unittest.TestCase):
     # For reading a data file with known input and solution.
     numberOfDataPoints      = 100;
@@ -45,15 +50,39 @@ class TestSegmentSignal(unittest.TestCase):
         names       = ["Depth", "Log", "SegmentedLog", "EventSequence", "FilteredLog"]
         self.data   = pd.read_csv(self.inputFile, sep="\t", header=None, names=names)
 
-        print(self.data.head())
 
-
-    def testResults(self):
-        results = Segment(list(self.data["Log"].values), len(self.data["Log"]), self.f, self.order, self.order1)
+    def testScalarResults(self):
+        logLength = len(self.data["Log"])
+        results   = Segment(list(self.data["Log"].values), logLength, self.f, self.order, self.order1)
 
         self.assertAlmostEqual(results.SegmentDensity, self.d_solution, places=5)
         # The variance of the jump sequence is not correct.  The reason is unknown.  See "SegmentSignalUnitTests.cs".
         #self.assertAlmostEqual(results.JumpSequenceVariance, self.c_solution, places=3)
+
+
+    def testArrayResults(self):
+        logLength = len(self.data["Log"])
+        results   = Segment(list(self.data["Log"].values), logLength, self.f, self.order, self.order1)
+
+        axis = plt.gca()
+        x = range(logLength)
+        axis.plot(x, results.FilteredSignal, color="red", label="Result Filtered Signal")
+        axis.plot(x, self.data["FilteredLog"], label="Input Filtered Signal", linestyle=(0, (5, 5)), linewidth=2)
+        axis.legend(loc="lower right", bbox_to_anchor=(1, 0), bbox_transform=axis.transAxes)
+        plt.show()
+
+        axis = plt.gca()
+        x = range(logLength)
+        axis.plot(x, results.SegmentedLog, color="red", label="Result Segmented Signal")
+        axis.plot(x, self.data["SegmentedLog"], label="Input Segmented Signal", linestyle=(0, (5, 5)), linewidth=2)
+        axis.legend(loc="lower right", bbox_to_anchor=(1, 0), bbox_transform=axis.transAxes)
+        plt.show()
+
+        delta = 0.3
+        for i in range(logLength):
+            self.assertAlmostEqual(results.SegmentedLog[i], self.data["SegmentedLog"].loc[i], delta=delta)
+            self.assertAlmostEqual(results.FilteredSignal[i], self.data["FilteredLog"].loc[i], delta=delta)
+
 
 
 if __name__ == "__main__":
